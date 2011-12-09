66e6a9d922337ad9be285efc0bd678d7c0747460
George MacKerron <george@mackerron.co.uk>
2011-12-09 22:16:36 +0000

    Now minifying unminified JS during script inlining

--- inline_scripts.rb
+++ inline_scripts.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
 html = open('source.html') { |f| f.read }
-html.gsub!(%r{<script src="[^/"]+"></script>}) { |tag| "<script>\n#{open(tag.match(/(?<=").+(?=")/)[0]) { |f| f.read }}\n</script>" }
+html.gsub!(%r{<script src="[^/"]+"></script>}) do |tag|
+  js = tag.match(/(?<=").+(?=")/)[0]
+  src = if js.match(/\.min\./)
+    open(js) { |f| f.read }
+  else
+    `java -jar ~/bin/closure-compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js '#{js}'`
+  end
+  "<script> // #{js}\n#{src.strip}\n</script>" 
+end
 open('index.html', 'w') { |f| f.write(html) }
